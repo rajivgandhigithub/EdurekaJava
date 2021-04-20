4cf7f6a402bb1d26d194bab78c79da734254848b
Rajivgandhi A <rajiv@test.com>
2021-04-20 17:25:37 +0300

    file added from rajivgandhi in hawally

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         int i;
         for (i = 0; i < 10; i++) {
-            System.out.println("Hello World");
+            System.out.println("Hello INDIA");
         }
     }
 }
